21da78fb856f6db11419233590f1e8093295479e
Jonathan Bry <jonathanbry@Jonathans-iMac.local>
2018-03-06 14:31:17 -0800

    commit Mar 6 14:36

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
+
 const express = require('express');
+
 const STATUS_USER_ERROR = 422;
 const OK = 200;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-let posts = [
+const posts = [
 
   {
     title: "Bill and Teds",
@@ -21,24 +23,26 @@ server.use(bodyParser.json());
 // to enable parsing of json bodies for post requests
 
 server.get('/posts', (req, res) => {
-  const good = "All Good!";
-  const bad = "Not Good!";
-  term = "Bill";
-for (i = 0; i < posts.length; i++) {
-  if (term === title[id] || contents[id]) {
-    return "All Good!";
-    res.status(200);
-    res.send(good);
+  const good = 'All Good!';
+  const bad = 'Not Good!';
+  const term = 'Bill';
+  for (i = 0; i < posts.length; i++) {
+    if (term === posts.title || term === posts.contents) {
+      return 'All Good!';
+      res.status(200);
+      res.send(good);
+    }
+    
 
-  } else if (term !== title[id] || contents[id]) {
-    return "Not Good!";
-    res.status(422);
-    res.send(bad);
+//   } else if (term !== title[] || contents[]) {
+//     return 'Not Good!';
+//     res.status(422);
+//     res.send(bad);
 
-  }
-}
+//   }
+// }
 
-});
+// });
 
 
 
